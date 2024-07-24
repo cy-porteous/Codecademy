60241d2afe47db7c52a728678432da6696cb68f4
Cy Porteous <cy.porteous@pm.me>
2024-07-24 17:49:21 +0100

    Add files via upload
    -m "further example of classes and instances added"

--- ClassesExample/ClassesExample/main.js
+++ ClassesExample/ClassesExample/main.js
@@ -55,6 +55,7 @@ doctorRoy.takeVacationDays(12);
 console.log(doctorRoy.remainingVacationDays);
 console.log(doctorRoy.insurance);
 
+// -------------------------------------------------------------------------------------------------------------------
 
 class Media {
   constructor(title){
@@ -97,7 +98,7 @@ class Book extends Media{
     return this._author;
   }
   get pages(){
-    return this.pages;
+    return this._pages;
   }
 }
 
@@ -130,3 +131,68 @@ speed.addRating(1);
 speed.addRating(1);
 speed.addRating(5);
 console.log(speed.getAverageRating());
+
+
+// -------------------------------------------------------------------------------------------------------
+
+class School {
+  constructor(name, level, numberOfStudents){
+    this._name = name;
+    this._level = level;
+    this._numberOfStudents = numberOfStudents;
+  }
+  get name(){
+    return this._name;
+  }
+  get level(){
+    return this._level;
+  }
+  get numberOfStudents(){
+    return this._numberOfStudents;
+  }
+  set numberOfStudents(value){
+    if (value.isNAN()) {
+      console.log('Invalid input: numberOfStudents must be set to a Number.')
+    }
+      else {
+        this._numberOfStudents = value;
+      }
+  }
+  quickFacts(){
+    console.log(`${this.name} educates ${this.numberOfStudents} students at the ${this.level} school level.`)
+  }
+  static pickSubstituteTeachers(substituteTeachers){
+     const randNum = Math.floor(Math.random() * substituteTeachers.length)
+      return substituteTeachers[randNum];
+  }
+}
+
+class PrimarySchool extends School{
+  constructor(name, numberOfStudents, pickupPolicy){
+    super(name, 'primary', numberOfStudents);
+    this._pickupPolicy = pickupPolicy;
+  }
+  get pickupPolicy(){
+    return this._pickupPolicy
+  }
+}
+
+class HighSchool extends School {
+  constructor(name, numberOfStudents, sportsTeams){
+    super(name, 'high', numberOfStudents);
+    this._sportsTeams = sportsTeams;
+  }
+  get sportsTeams(){
+    return this._sportsTeams;
+  }
+}
+
+const lorraineHansbury = new PrimarySchool('Lorraine Hansbury', 514, 'Students must be picked up by a parent, guardian, or a family member over the age of 13')
+
+lorraineHansbury.quickFacts();
+const subTeacher = School.pickSubstituteTeachers(['Jamal Crawford', 'Lou Williams', 'J. R. Smith', 'James Harden', 'Jason Terry', 'Manu Ginobli']);
+
+const alSmith = new HighSchool ('Al E.Smith', 415, ['Baseball', 'Basketball', 'Volleyball', 'Track and Field']);
+
+console.log(alSmith.sportsTeams)
+console.log(subTeacher);
